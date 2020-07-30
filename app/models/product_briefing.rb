ad2f062dc8d5eb5c447ac5f7a7e3b4fa1fb03bc0
DanielCortild <daniel.cortild@gmail.com>
2020-07-30 11:58:20 +0200

    Model Separation

--- app/models/product_briefing.rb
+++ app/models/product_briefing.rb
@@ -1,11 +1,3 @@
 class ProductBriefing < ApplicationRecord
   has_one_attached :file
 end
-
-class Whitepaper < ApplicationRecord
-  has_one_attached :file
-end
-
-class Faq < ApplicationRecord
-  has_one_attached :file
-end
