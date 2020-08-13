22dc2ccbedd13c3901b50a4b804de957df6cf691
DanielCortild <daniel.cortild@gmail.com>
2020-08-13 12:22:02 +0200

    Added News

--- app/mailers/user_response_mailer.rb
+++ app/mailers/user_response_mailer.rb
@@ -1,17 +1,11 @@
 class UserResponseMailer < ApplicationMailer
   def new_downloaded_file_email
     @downloaded_file = params[:downloaded_file]
-    mail(to: receiverAddress, subject: "[#{Time.now.to_i}] New download of \"#{@downloaded_file.file}\"")
+    mail(to: ENV['EDIFLY_RECEIVER_EMAIL'], subject: "[#{Time.now.to_i}] New download of \"#{@downloaded_file.file}\"")
   end
   def new_message_email
     @message = params[:message]
-    mail(to: receiverAddress, subject: "[#{Time.now.to_i}] New message about #{@message.concerning}")
+    mail(to: ENV['EDIFLY_RECEIVER_EMAIL'], subject: "[#{Time.now.to_i}] New message about #{@message.concerning}")
   end
-
-  private
-
-    def receiverAddress
-      ENV['EDIFLY_RECEIVER_EMAIL']
-    end
 
 end
