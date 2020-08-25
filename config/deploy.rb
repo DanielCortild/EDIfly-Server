11bc75026f7d1396e915a257140f138fb2f9c7e8
DanielCortild <daniel.cortild@gmail.com>
2020-08-25 15:14:18 +0200

    Databases

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,14 +3,14 @@ lock "~> 3.14.1"
 require 'capistrano-db-tasks'
 
 set :application, "edifly-server"
-set :repo_url
-set :deploy_to, "/var/www/edifly-server"
+set :repo_url, 'git@github.com:DanielCortild/EDIfly-Server.git'
+set :deploy_to, "/home/deploy/edifly-server"
 
 set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 3
-set :keep-assets, 3
+set :keep_assets, 3
 
 set :db_local_clean, true
 set :db_remote_clean, true
