cf39f33143847dd499d08329cf4da6ebfd4539c5
DanielCortild <daniel.cortild@gmail.com>
2020-08-26 19:05:01 +0200

    Host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'edifly.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'x.edifly.com' }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
