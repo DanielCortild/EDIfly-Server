fca13217fdcd5fabd715f790fa0b1dc229b67a49
DanielCortild <daniel.cortild@gmail.com>
2020-08-27 09:27:11 +0200

    Changed credentials

--- app/mailers/user_response_mailer.rb
+++ app/mailers/user_response_mailer.rb
@@ -1,11 +1,17 @@
 class UserResponseMailer < ApplicationMailer
   def new_downloaded_file_email
     @downloaded_file = params[:downloaded_file]
-    mail(to: ENV['EDIFLY_RECEIVER_EMAIL'], subject: "[#{Time.now.to_i}] New download of \"#{@downloaded_file.file}\"")
+    mail(to: receiver_email, subject: "[#{Time.now.to_i}] New download of \"#{@downloaded_file.file}\"").deliver_now
   end
   def new_message_email
     @message = params[:message]
-    mail(to: ENV['EDIFLY_RECEIVER_EMAIL'], subject: "[#{Time.now.to_i}] New message about #{@message.concerning}")
+    mail(to: receiver_email, subject: "[#{Time.now.to_i}] New message about #{@message.concerning}")
+  end
+
+  private
+
+  def receiver_email
+    Rails.application.credentials.production[:receiver_gmail_username]
   end
 
 end
