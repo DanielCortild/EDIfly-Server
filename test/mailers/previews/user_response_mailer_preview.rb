354cb8b7048e0643f2e2b5648fa67cd000006b29
DanielCortild <daniel.cortild@gmail.com>
2020-09-07 13:43:47 +0200

    Set up downloading file message

--- test/mailers/previews/user_response_mailer_preview.rb
+++ test/mailers/previews/user_response_mailer_preview.rb
@@ -4,6 +4,10 @@ class UserResponseMailerPreview < ActionMailer::Preview
     downloaded_file = DownloadedFile.new(name: "Joe Smith", company: "EDIfly", email: "example@email.com", file: "faq")
     UserResponseMailer.with(downloaded_file: downloaded_file).new_downloaded_file_email
   end
+  def new_downloaded_file_email_client
+    downloaded_file = DownloadedFile.new(name: "Joe Smith", company: "EDIfly", email: "example@email.com", file: "faq")
+    UserResponseMailer.with(downloaded_file: downloaded_file).new_downloaded_file_email_client
+  end
   def new_message_email
     message = Message.new(name: "Joe Smith", company: "EDIfly", email: "joe.smith@email.com", concerning: "Something Interesting", message: "I want to place an order!")
     UserResponseMailer.with(message: message).new_message_email
